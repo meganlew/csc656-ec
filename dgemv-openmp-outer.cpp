96b4132a806d9a93c4513f24069433ffb7b696a7
meganlew <meganlew4@gmail.com>
2022-12-09 15:59:45 -0800

    test

--- dgemv-openmp-outer.cpp
+++ dgemv-openmp-outer.cpp
@@ -27,7 +27,6 @@ void my_dgemv(int n, double* A, double* x, double* y) {
  
    #pragma omp parallel for 
    for(int i = 0; i < n; i++){
-      #pragma omp parallel for 
       for(int j = 0; j < n; j++){
          y[i] += A[i*n+j] * x[j];
       }
